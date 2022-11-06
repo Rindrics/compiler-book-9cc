5fe1f05ad65496a69ac0eff475a49e44ed3a1a8b
Akira Hayashi <smxshxishxad@me.com>
2022-11-06 22:01:28 +0900

    :wrench: Enumerate TokenKind

--- 9cc.c
+++ 9cc.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "unexpected number of arguments");
