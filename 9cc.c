45bdd321819f1f6cc8090b5f642ca92a76e9d059
Akira Hayashi <smxshxishxad@me.com>
2022-11-06 22:51:11 +0900

    :recycle: Generate error message using `error()`

--- 9cc.c
+++ 9cc.c
@@ -93,7 +93,7 @@ Token *tokenize(char *p) {
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "unexpected number of arguments");
+    error("unexpected number of arguments");
     return 1;
   }
 
